8405f544913dc6d2688d175112e9f8900b008a0d
Marcus Runge <marcus.runge@outlook.com>
2020-07-06 18:38:51 +0200

    REFACTORED data

--- data/src/main/kotlin/com/marcusrunge/roominfo/data/bases/DataBase.kt
+++
@@ -1,6 +0,0 @@
-package com.marcusrunge.roominfo.data.bases
-
-import android.content.Context
-import com.marcusrunge.roominfo.data.interfaces.Data
-
-internal abstract class DataBase(context: Context) : Data
--- data/src/main/kotlin/com/marcusrunge/roominfo/data/interfaces/AgendaItems.kt
+++ data/src/main/kotlin/com/marcusrunge/roominfo/data/interfaces/AgendaItems.kt
@@ -2,11 +2,15 @@ package com.marcusrunge.roominfo.data.interfaces
 
 import androidx.room.*
 import com.marcusrunge.roominfo.data.models.AgendaItem
+import java.time.OffsetDateTime
 
 @Dao
 interface AgendaItems {
     @Query("SELECT * FROM agendaitem")
     fun getAll(): List<AgendaItem>
+
+    @Query("SELECT * FROM agendaitem WHERE `end`> :dateTime LIMIT 3")
+    fun getThree(dateTime: OffsetDateTime): List<AgendaItem>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(agendaItem: AgendaItem)
