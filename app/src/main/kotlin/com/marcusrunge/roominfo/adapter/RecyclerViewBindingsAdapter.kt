f7451805f9f87b0cb271c7945cff95401680c33b
Marcus Runge <marcus.runge@outlook.com>
2020-07-19 18:36:39 +0200

    ADDED RecyclerViewBindingsAdapter

---
+++ app/src/main/kotlin/com/marcusrunge/roominfo/adapter/RecyclerViewBindingsAdapter.kt
@@ -0,0 +1,29 @@
+package com.marcusrunge.roominfo.adapter
+
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.marcusrunge.roominfo.R
+
+
+object RecyclerViewBindingsAdapter {
+    @BindingAdapter("setAdapter")
+    @JvmStatic
+    fun bindAdapter(recyclerView: RecyclerView, adapter: RecyclerView.Adapter<*>?) {
+        recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
+        recyclerView.adapter = adapter
+        val divider = DividerItemDecoration(
+            recyclerView.context,
+            DividerItemDecoration.VERTICAL
+        )
+        divider.setDrawable(
+            ContextCompat.getDrawable(
+                recyclerView.context,
+                R.drawable.shape_transparentlinedivider
+            )!!
+        )
+        recyclerView.addItemDecoration(divider)
+    }
+}
--- app/src/main/kotlin/com/marcusrunge/roominfo/ui/calendar/CalendarViewModel.kt
+++ app/src/main/kotlin/com/marcusrunge/roominfo/ui/calendar/CalendarViewModel.kt
@@ -1,9 +1,23 @@
 package com.marcusrunge.roominfo.ui.calendar
 
 import android.content.Context
+import androidx.databinding.Bindable
+import androidx.databinding.library.baseAdapters.BR
+import com.marcusrunge.roominfo.adapter.AgendaRecyclerViewAdapter
+import com.marcusrunge.roominfo.models.AgendaItem
 import com.marcusrunge.roominfo.ui.ViewModelBase
 import javax.inject.Inject
 
 class CalendarViewModel @Inject constructor(
     private val context: Context
-) : ViewModelBase()
+) : ViewModelBase() {
+    private val agendaItems: MutableList<AgendaItem> = mutableListOf()
+
+    @get:Bindable
+    var agendaRecyclerViewAdapter: AgendaRecyclerViewAdapter? =
+        AgendaRecyclerViewAdapter(agendaItems)
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.agendaRecyclerViewAdapter)
+        }
+}
