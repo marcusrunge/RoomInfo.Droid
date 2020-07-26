a5533e4443796ae8cf2aa5f59917afb953ad3c84
Marcus Runge <marcus.runge@outlook.com>
2020-07-26 16:56:19 +0200

    ADDED methods

--- app/src/main/kotlin/com/marcusrunge/roominfo/ui/calendar/CalendarViewModel.kt
+++ app/src/main/kotlin/com/marcusrunge/roominfo/ui/calendar/CalendarViewModel.kt
@@ -41,4 +41,67 @@ class CalendarViewModel @Inject constructor(
             )
         }
     }
+
+    fun showAddAgendaItemDialog() {
+
+    }
+
+    private fun addAgendaItem(agendaItem: AgendaItem) {
+        agendaItems.add(agendaItem)
+        data.agendaItems.insert(
+            com.marcusrunge.roominfo.data.models.AgendaItem(
+                0,
+                agendaItem.title,
+                agendaItem.start,
+                agendaItem.end,
+                agendaItem.allDayEvent,
+                agendaItem.overridden,
+                agendaItem.description,
+                agendaItem.occupancy,
+                agendaItem.timeStamp,
+                agendaItem.deleted
+            )
+        )
+    }
+
+    private fun updateAgendaItem(agendaItem: AgendaItem) {
+        for (i in agendaItems.indices) {
+            if (agendaItems[i].id == agendaItem.id) {
+                agendaItems[i].title = agendaItem.title
+                agendaItems[i].start = agendaItem.start
+                agendaItems[i].end = agendaItem.end
+                agendaItems[i].allDayEvent = agendaItem.allDayEvent
+                agendaItems[i].overridden = agendaItem.overridden
+                agendaItems[i].description = agendaItem.description
+                agendaItems[i].occupancy = agendaItem.occupancy
+                agendaItems[i].timeStamp = agendaItem.timeStamp
+                agendaItems[i].deleted = agendaItem.deleted
+                break
+            }
+        }
+        data.agendaItems.update(
+            com.marcusrunge.roominfo.data.models.AgendaItem(
+                agendaItem.id!!,
+                agendaItem.title,
+                agendaItem.start,
+                agendaItem.end,
+                agendaItem.allDayEvent,
+                agendaItem.overridden,
+                agendaItem.description,
+                agendaItem.occupancy,
+                agendaItem.timeStamp,
+                agendaItem.deleted
+            )
+        )
+    }
+
+    fun deleteAgendaItem(id: Long) {
+        for (i in agendaItems.indices) {
+            if (agendaItems[i].id == id) {
+                agendaItems.removeAt(i)
+                break
+            }
+        }
+        data.agendaItems.delete(id.toInt())
+    }
 }
